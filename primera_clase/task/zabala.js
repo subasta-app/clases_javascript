2552195478c192c3c5a4865ca8d9f412fd6e1ccb
Manuel Zabala <cristianzabala96@gmail.com>
2023-01-03 21:33:32 -0500

    add: task javascript

---
+++ primera_clase/task/zabala.js
@@ -0,0 +1,37 @@
+//primer tarea completar los console logs con los otros datos
+let cadenasTexto = 'anita lava la tina';
+let numeros = 2;
+let booleanos = true;
+let arreglos = [1,2,cadenasTexto,numeros,booleanos,false,'chao']
+
+console.log('Que tipo de dato es una cadena de texto: ' + typeof(cadenasTexto) + ' y el dato es: '+ cadenasTexto);
+console.log('Que tipo de dato es un numero: ' + typeof(numeros) + ' y el dato es: '+ numeros);
+console.log('Que tipo de dato es un booleano: ' + typeof(booleanos) + ' y el dato es: '+ booleanos);
+console.log('Que tipo de dato es un arreglo: ' + typeof(arreglos) + ' y el dato es: '+ arreglos);
+
+// tarea utilizar los siguientes operadores aritmeticos
+// + , - , / , * , ** , %
+
+let suma = 6+3;
+
+console.log(suma);
+
+let resta = 10-5;
+
+console.log(resta);
+
+let division = 20/5;
+
+console.log(division);
+
+let multiplicacion = 7*6;
+
+console.log(multiplicacion);
+
+let potenciacion = 3**5;
+
+console.log(potenciacion);
+
+let residuo = 5%3;
+
+console.log(residuo);
