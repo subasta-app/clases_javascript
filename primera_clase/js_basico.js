c50b6419d3340ded786609a3b7cb653ac119cc38
oskarbikes <oskarbmx12@hotmail.com>
2023-01-04 14:50:38 -0500

    first_task_oscar

--- primera_clase/js_basico.js
+++ primera_clase/js_basico.js
@@ -16,11 +16,28 @@ let arreglos = [1,2,cadenasTexto,numeros,booleanos,false,'chao']
 
 console.log('Que tipo de dato es una cadena de texto: ' + typeof(cadenasTexto) + ' y el dato es: '+ cadenasTexto);
 console.log('Que tipo de dato es un numero: ' + typeof(numeros) + ' y el dato es: '+ numeros);
+console.log('Que tipo de dato es un booleano: ' + typeof(booleanos)+ 'y el dato es: '+ booleanos);
+console.log('Que tipo de dato es un arreglo: '+ typeof(arreglos)+ 'y el dato es:'+ arreglos);
 
 //primer tarea completar los console logs con los otros datos
 
 let sumaDeDosNumeros = 6 + 3;
 console.log('Resultado: ' + sumaDeDosNumeros);
+
+let restaDeDosNumeros = 7 - 4;
+console.log('Resultado: '+ restaDeDosNumeros);
+
+let divisionDeDosNumeros = 20 / 4;
+console.log('resultado: '+ divisionDeDosNumeros);
+
+let multiplicacionDeDosNumeros = 9 * 9;
+console.log('Resultado: '+ multiplicacionDeDosNumeros);
+
+let exponenteDeUnNumero = 5 ** 5;
+console.log('Resultado: '+ exponenteDeUnNumero);
+
+let potenciaDeUnNumero = 10 % 2;
+console.log('Resultado: '+ potenciaDeUnNumero);
 
 // tarea utilizar los siguientes operadores aritmeticos
 // + , - , / , * , ** , %
