013b54a30529f202a7b072beacaddd3f5acc750d
Edwin Bikes <edwinbykes@hotmail.com>
2023-01-06 16:12:42 -0500

    cargue de tarea

---
+++ primera_clase/task/edwinbikes.js
@@ -0,0 +1,40 @@
+console.log('Hola Mundo 2023');
+
+let cadenasTexto = 'anita lava la tina';
+let numeros = 2;
+let booleanos = true;
+let arreglos = [1, 2, cadenasTexto, numeros, booleanos, false, 'chao']
+
+console.log('Que tipo de dato es una cadena de texto: ' + typeof (cadenasTexto) + ' y el dato es: ' + cadenasTexto);
+console.log('Que tipo de dato es un numero: ' + typeof (numeros) + ' y el dato es: ' + numeros);
+
+
+
+//primer tarea completar los console logs con los otros datos
+console.log(' ');
+console.log('tarea 1');
+console.log('tipos de datos Booleanos: ' + typeof (booleanos) + ' los datos son: ' + booleanos);
+console.log('tipos de datos de arreglos: ' + typeof (arreglos) + ' los datos son:' + arreglos);
+
+
+let sumaDeDosNumeros = 6 + 3;
+console.log('Resultado: ' + sumaDeDosNumeros);
+
+// tarea utilizar los siguientes operadores aritmeticos
+// + , - , / , * , ** , %
+
+let resta = 3 - 6;
+let division = 3 / 6;
+let multiplicacion = 3 * 6;
+let potencia = 3 ** 6;
+let residuo = 3 % 6;
+
+console.log(' ');
+console.log('tarea 2');
+
+console.log(`1. Resultado multiplicando : ` + multiplicacion);
+console.log('1. Resultado restando: ' + resta);
+console.log('1. Resultado dividiendo: ' + division);
+console.log('1. Resultado potenciacion: ' + potencia);
+console.log('1. Resultado residuo: ' + residuo);
+
