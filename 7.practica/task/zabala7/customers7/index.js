3961cd948c25278f52e7d373f72508a851d30139
Manuel Zabala <cristianzabala96@gmail.com>
2023-01-27 18:56:54 -0500

    feat: add task class 7 and 9

---
+++ 7.practica/task/zabala7/customers7/index.js
@@ -0,0 +1,17 @@
+class Customer{
+    constructor(customer){     
+        this.dataCustomer = this.organizeData(customer)
+    }
+
+    organizeData(customer){
+        return {
+            "name":customer.name,
+            "phone":customer.phone,
+            "address":customer.address,
+            "legal_id_type":customer.legal_id_type,
+            "legal_id":customer.legal_id
+        }
+    }
+}
+
+export default Customer
