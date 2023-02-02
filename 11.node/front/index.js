d6d225bdecc43e651e88fc72e81dbe9148ceff8a
Juan Espitia <juanespitia@outlook.com>
2023-02-01 20:48:52 -0500

    feat: front to users api

--- 11.node/front/index.js
+++ 11.node/front/index.js
@@ -1,13 +1,15 @@
-async function llamandoARick() {
-    let response = await fetch('http://127.0.0.1:3000/cars')
+async function getUsers() {
+    let response = await fetch('http://127.0.0.1:3000/users')
         .then((response) => response.json())
     let charactersHtml = document.getElementById("characters")
 
     let insertHtml = '';
 
-    characters = response.data
+    console.log(response)
+
+    characters = response
     characters.forEach(character => {
-        let newElementHtml = `<div class="container"><div class="name" ><p> ${character.name} <p></div><div class="image"> <img class="image__car" src="${character.image}" alt=""></div></div> `
+        let newElementHtml = `<div class="container"><div class="name" ><p> ${character.name} <p></div><div class="name" ><p> ${character.email} <p></div></div> `
         insertHtml = insertHtml + newElementHtml
     })
 
@@ -16,12 +18,47 @@ async function llamandoARick() {
 // Verbos HTTP -> protocolo de comunicacion en internet (hyper text transfer protocol)
 // GET POST PATCH PUT DELETE diferencia en concepto a nivel de put y patch
 // CRUD  Create Read Update Delete
-async function showDetails(id){
-    let character = await fetch('https://rickandmortyapi.com/api/character/'+id)
-        .then((response) => response.json())
-    alert(
-        `Mi nombre es ${character.name} soy del planeta ${character.origin.name} y vivo en ${character.location.name}`
-    )
-}
 
-llamandoARick()
+
+
+
+
+// Ejemplo implementando el metodo POST:
+async function postData() {
+    let url = 'http://127.0.0.1:3000/users'
+    let id = document.getElementById('idValue').value
+    let name = document.getElementById('name').value
+    let email = document.getElementById('email').value
+    let data = {
+        data:{
+            id: id,
+            name: name,
+            email:email
+        }
+    }
+    console.log(data);
+    // Opciones por defecto estan marcadas con un *
+    
+    try {
+        const response = await fetch(url, {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+              'Content-Type': 'application/json'
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+          });
+          return location.reload(); // parses JSON response into native JavaScript objects
+    } catch (error) {
+        alert(error)
+        return location.reload();
+    }
+   
+  }
+  
+  getUsers()
